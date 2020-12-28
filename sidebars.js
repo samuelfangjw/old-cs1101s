9f3fc04ea0ac67c3df6e7dffc61d23ed9ab734f9
Samuel Fang <samuelfangjw@gmail.com>
2020-12-28 19:42:20 +0800

    added higher order functions and recursion

--- sidebars.js
+++ sidebars.js
@@ -5,7 +5,18 @@ module.exports = {
       id: 'about'
     },
     {
-      Review: ['review/s2', 'review/s3', 'review/s4', 'review/s5', 'review/s6', 'review/s9', 'review/s10', 'review/s11', 'review/s12'],
+      type:'doc',
+      id: 'review/s2'
+    },
+    {
+      type: 'doc',
+      id: 'review/s3'
+    },
+    {
+      'Studio S4: Recursion and Higher Order Functions': ['review/recursion', 'review/higherorderfunctions', 'review/substitutionmodel']
+    },
+    {
+      Review: ['review/s5', 'review/s6', 'review/s9', 'review/s10', 'review/s11', 'review/s12'],
       // Extra: ['doc1'],
       // Docusaurus: ['doc1', 'doc2', 'doc3'],
       // Features: ['mdx'],
